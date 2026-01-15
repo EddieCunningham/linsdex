2fb68672f62346a38bff7f3f8dafd3f2e264f9b3
EddieCunningham <edmondcunnin@umass.edu>
2026-01-14 21:11:18 -0500

    bug fix

--- linsdex/matrix/__init__.py
+++ linsdex/matrix/__init__.py
@@ -3,5 +3,3 @@ from linsdex.matrix.dense import *
 from linsdex.matrix.diagonal import *
 from linsdex.matrix.block import *
 from linsdex.matrix.matrix_with_inverse import *
-from linsdex.matrix.butterfly import *
-from linsdex.matrix.butterfly_channel import *
